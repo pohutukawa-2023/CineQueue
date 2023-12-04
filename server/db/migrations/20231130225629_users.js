7451636160b6c905f341943bbf0daf285b626e47
Vitor <142807308+vitor-fernandess@users.noreply.github.com>
2023-12-05 10:47:05 +1300

    db table changes

--- server/db/migrations/20231130225629_users.js
+++ server/db/migrations/20231130225629_users.js
@@ -1,6 +1,5 @@
 export async function up(knex) {
   await knex.schema.createTable('users', (table) => {
-    // table.increments('id').primary()
     table.string('auth_id').primary()
     table.string('genre')
     table.integer('time_to_watch')
